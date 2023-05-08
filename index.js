8664b01c5cc8222d38e67321eb0c9003b259a5e5
ravibangundi <ravibangundi1999@gmail.com>
2023-05-08 14:00:31 +0530

    added all files

---
+++ index.js
@@ -0,0 +1,9 @@
+
+// var $pro = $("span");
+// $pro.scroll(function (){
+//     setTimeout(function(){$pro.Toggle();},1000);
+    
+//     // $pro.fadeIn();
+// });
+
+// const projects = document.querySelector(".projects");
